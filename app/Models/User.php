74c93beb0f3cda292c97afb36c49c690ca22c695
adi <adi.benh9@gmail.com>
2025-09-11 16:08:27 +0200

    nieuwe migration om het te laten werken met de rollen

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Models\Role;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -33,6 +33,9 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    public function roles() {
+        return $this->belongsToMany(Role::class);
+    }
     /**
      * Get the attributes that should be cast.
      *
